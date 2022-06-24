92474764a697b6c90d34a9f24faa0d4e676b4134
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-06-24 23:11:06 +0530

    Time: 51 ms (69.01%), Space: 13.9 MB (80.39%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.py
+++ 377-combination-sum-iv/377-combination-sum-iv.py
@@ -3,6 +3,6 @@ class Solution:
         nums, combs = sorted(nums), [1] + [0] * (target)
         for i in range(target + 1):
             for num in nums:
-                if num  > i: break
-                combs[i] += combs[i - num]
+                if num  <=i:
+                    combs[i] += combs[i - num]
         return combs[target]
