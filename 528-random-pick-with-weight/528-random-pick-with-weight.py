615ba54a8be54b8cfe668831d1880dcb33e5ae76
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-06-24 23:25:24 +0530

    Time: 7194 ms (11.62%), Space: 18.6 MB (41.20%) - LeetHub

---
+++ 528-random-pick-with-weight/528-random-pick-with-weight.py
@@ -0,0 +1,41 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.w = w
+		# 1. calculate relative frequency
+        denom = sum(self.w)
+        for i in range(len(self.w)):
+            self.w[i] = self.w[i] / denom
+        # 2. put relative frequencies on the number line between 0 and 1
+		# Note self.w[-1] = 1
+        for i in range(1,len(self.w)):
+            self.w[i] += self.w[i-1]
+        
+    def pickIndex(self) -> int:
+		
+		# this is where we pick the index
+        N = random.uniform(0,1)
+      
+        flag = 1
+        index = -1
+        
+		# test each region of the numberline to see if N falls in it, if it 
+		# does not then go to the next index and check if N falls in it
+		# this is gaurenteed to break because of previous normalization
+        while flag:
+            index +=1 
+           
+           
+     
+            if N <= self.w[index]:
+                flag = 0
+        
+    
+        return index
+
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
