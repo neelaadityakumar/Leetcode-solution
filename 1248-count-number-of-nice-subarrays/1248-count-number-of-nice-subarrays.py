8573ba08eda1762d7e9c50213c317e93f765a2ac
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-06-26 17:40:59 +0530

    Time: 1673 ms (9.32%), Space: 19.2 MB (98.62%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -2,31 +2,17 @@ class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
         # This is just converting the even number to 0 and odd numbers to 1 in the array
         for i in range(len(nums)):
-
-            if nums[i] % 2 == 0:
-                nums[i] = 0
-            else:
-                nums[i] = 1
-
+            nums[i] = nums[i]&1
         # Then doing exactly the same thing as subarray sum equal k problem
 
         currsum = 0
         subarray = 0
-        hashmap = {}
+        hashmap = {0:1}
 
         for num in nums:
             currsum += num
-
-            if currsum == k:
-                subarray += 1
-
             if currsum - k in hashmap:
                 subarray += hashmap[currsum - k]
-
-            if currsum in hashmap:
-                hashmap[currsum] += 1
-
-            else:
-                hashmap[currsum] = 1
+            hashmap[currsum]=hashmap.get(currsum,0)+1
 
         return subarray
