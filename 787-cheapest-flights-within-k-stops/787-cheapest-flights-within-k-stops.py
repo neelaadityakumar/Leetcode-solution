445ca25563c1444903f982e485b7de0275c94eab
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-06-29 23:19:02 +0530

    Time: 214 ms (57.77%), Space: 15.3 MB (37.15%) - LeetHub

--- 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.py
@@ -1,30 +1,29 @@
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
-        price_table = [ float('inf') for _ in range(n) ]
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        #Make graph
+        adj_list = {i:[] for i in range(n)}
+        for frm, to, price in flights:
+            adj_list[frm].append((to, price))
         
-        # price of source must be 0
-        price_table[ src ] = 0
-		
-        # initialization with 0 transfer
-        for source, destination, ticket_price in flights:
-            if source == src:
-                price_table[destination] = ticket_price
+        best_visited = [2**31]*n # Initialized to maximum
         
-        
-        # tranfer k times to update price table
-        for trasfer in range(0, K):
+        prior_queue = [ (0, -1, src) ]  # weight, steps, node
+
+        while prior_queue:
+            cost, steps, node = heapq.heappop(prior_queue)
             
-            current_price = [*price_table]
+            if best_visited[node] <= steps:  # Have seen the node already, and the current steps are more than last time
+                continue
+
+            if steps > k:  # More than k stops, invalid
+                continue
+
+            if node==dst:  # reach the destination # as priority_queue is a minHeap so this cost is the most minimum cost.
+                return cost
             
-            for source, destination, ticket_price in flights:
-                
-                current_price[destination] = min(current_price[destination], price_table[source] + ticket_price )
-            
-            # update current price back to price table    
-            price_table = current_price
-        
-        
-        if price_table[dst] == float('inf'):
-            return -1
-        else:
-            return price_table[dst]
+            best_visited[node] = steps # Update steps
+
+            for neighb, weight in adj_list[node]:
+                heapq.heappush(prior_queue, (cost + weight, steps + 1, neighb))
+
+        return -1
