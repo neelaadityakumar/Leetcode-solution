bba0cc8eacbf3727caf63d3b030321b5d5401a02
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-06-29 22:18:19 +0530

    Time: 3549 ms (41.43%), Space: 65.8 MB (95.76%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -8,21 +8,22 @@ class Solution:
             network[a].append(b)
             network[b].append(a)
         
-        def tarjan(prev, node, time, disc, low, network, ans):
-            if disc[node] != -1:
-                return disc[node]
+        def tarjan(prev, curr, time, disc, low, network, ans):
+            if disc[curr] != -1:
+                return disc[curr]
 
-            disc[node] = time
-            low[node] = time
+            disc[curr] = time
+            low[curr] = time
             
-            for x in network[node]:
-                if x != prev:
-                    newLow = tarjan(node, x, time+1, disc, low, network, ans)
-                    low[node] = min(low[node], newLow)
-                    if low[x] > disc[node]:
-                        ans.append([x,node])
+            for next in network[curr]:
+                if next != prev:
+                    lowOfNext = tarjan(curr, next, time+1, disc, low, network, ans)
+                    low[curr] = min(low[curr], lowOfNext)
+#after backtracking, the value of low[next] is still higher than disc[curr], then there is no looped #connection, meaning that the edge between curr and next is a bridge, so we should add it to our answer #array (ans).
+                    if low[next] > disc[curr]:
+                        ans.append([next,curr])
                         
-            return low[node]
+            return low[curr]
         
         ans = []
         tarjan(0, 0, 0, disc, low, network, ans)
