3f4a5870d60d3c8c8b93c0e1fd63bc9a26e8efbf
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-06-29 22:39:44 +0530

    Time: 1547 ms (8.18%), Space: 18.9 MB (67.33%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+        trusts = [0] * (N+1)
+        for (a, b) in trust:
+            trusts[a] -= 1
+            #If he trust some one reduce 1
+            #else add 1
+            trusts[b] += 1
+            
+        for i in range(1, len(trusts)):
+            if trusts[i] == N-1:
+                return i
+        return -1
