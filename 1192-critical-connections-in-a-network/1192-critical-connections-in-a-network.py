117356889551ea840b216d8a0d8799d6a1652048
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-06-29 22:22:28 +0530

    Time: 3335 ms (53.09%), Space: 65.7 MB (97.76%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.py
@@ -1,31 +1,31 @@
 class Solution:
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
-        disc = [-1]*n
-        low = [-1]*n
+        discoveryTime = [-1]*n
+        lowestNodeReachable = [-1]*n
         
         network = [[] for _ in range(n)]
         for a, b in connections:
             network[a].append(b)
             network[b].append(a)
         
-        def tarjan(prev, curr, time, disc, low, network, ans):
-            if disc[curr] != -1:
-                return disc[curr]
+        def tarjan(prev, curr, time, discoveryTime, lowestNodeReachable, network, ans):
+            if discoveryTime[curr] != -1:
+                return discoveryTime[curr]
 
-            disc[curr] = time
-            low[curr] = time
+            discoveryTime[curr] = time
+            lowestNodeReachable[curr] = time
             
             for next in network[curr]:
                 if next != prev:
-                    lowOfNext = tarjan(curr, next, time+1, disc, low, network, ans)
-                    low[curr] = min(low[curr], lowOfNext)
+                    lowOfNext = tarjan(curr, next, time+1, discoveryTime, lowestNodeReachable, network, ans)
+                    lowestNodeReachable[curr] = min(lowestNodeReachable[curr], lowOfNext)
 #after backtracking, the value of low[next] is still higher than disc[curr], then there is no looped #connection, meaning that the edge between curr and next is a bridge, so we should add it to our answer #array (ans).
-                    if low[next] > disc[curr]:
+                    if lowestNodeReachable[next] > discoveryTime[curr]:
                         ans.append([next,curr])
                         
-            return low[curr]
+            return lowestNodeReachable[curr]
         
         ans = []
-        tarjan(0, 0, 0, disc, low, network, ans)
+        tarjan(0, 0, 0, discoveryTime, lowestNodeReachable, network, ans)
         
         return ans
