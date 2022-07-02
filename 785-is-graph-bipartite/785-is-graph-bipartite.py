05d08527b20e335ed3a7acc9f452fa804793d5ab
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-07-02 18:36:16 +0530

    Time: 331 ms (24.36%), Space: 14.3 MB (92.15%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -7,15 +7,16 @@ class Solution:
         for node in range(len(graph)):
             if node not in color:
                 color[node] = 1
-                #visiting with 0 color initially because it is initial or disconnected
+                #visiting with 0  or 1 color initially because it is initial or disconnected
                 q.append(node)
                 while len(q):
                     curr = q.pop(0)
                     for adj_node in graph[curr]:
-                        if color[adj_node] == color[curr]:
-                            return False
+                        
                         if color[adj_node] == -1:
                             color[adj_node] = 1-color[curr] 
                             #alternate color
                             q.append(adj_node)
+                        elif color[adj_node] == color[curr]:
+                            return False
         return True
