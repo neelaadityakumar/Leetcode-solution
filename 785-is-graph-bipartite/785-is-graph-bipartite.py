48c0bc5c8970072d93a05ae5698c4074638d40a0
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-07-02 18:32:54 +0530

    Time: 196 ms (84.45%), Space: 14.4 MB (75.63%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.py
+++ 785-is-graph-bipartite/785-is-graph-bipartite.py
@@ -1,19 +1,21 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        # white -> uncoloured
-        table = defaultdict(lambda: -1)
+        # -1 -> uncoloured
+        color = defaultdict(lambda: -1)
         q = []
 		# just in case graph is disconnected
         for node in range(len(graph)):
-            if node not in table:
-                table[node] = 0
+            if node not in color:
+                color[node] = 0
+                #visiting with 0 color initially
                 q.append(node)
                 while len(q):
                     curr = q.pop(0)
                     for adj_node in graph[curr]:
-                        if table[adj_node] == table[curr]:
+                        if color[adj_node] == color[curr]:
                             return False
-                        if table[adj_node] == -1:
-                            table[adj_node] = 1 if table[curr] == 0 else 0
+                        if color[adj_node] == -1:
+                            color[adj_node] = 1-color[curr] 
+                            #alternate color
                             q.append(adj_node)
         return True
