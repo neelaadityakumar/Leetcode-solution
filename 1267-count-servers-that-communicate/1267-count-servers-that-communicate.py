e338367a58aa5ee648d6d25682613d9fe69c0798
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-07-03 14:06:19 +0530

    Time: 3292 ms (5.22%), Space: 19.4 MB (10.33%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -5,8 +5,6 @@ class Solution:
         for row in range(len(grid)):
             for col in range(len(grid[0])):
                 potential_servers = 0 
-                # if grid[row][col] == 1 and (row, col) not in seen:
-                #     seen.add((row, col))
                 potential_servers = self.four_directional_search(grid, row, col, seen)
                 if potential_servers > 1:
                     number_of_servers += potential_servers 
