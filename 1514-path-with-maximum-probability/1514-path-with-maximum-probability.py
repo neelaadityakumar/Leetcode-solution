dd74e2facb9f3ee7f20c813d84a2431b11f4c665
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-07-05 01:16:11 +0530

    Time: 1577 ms (9.32%), Space: 29.4 MB (5.11%) - LeetHub

---
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
+        g=defaultdict(list)
+        
+        for edge, prob in zip(edges, succProb):
+            u,v, cost = edge[0], edge[1], prob
+            g[u].append([v, log2(1/cost)])
+            g[v].append([u, log2(1/cost)])
+            
+        def dijkstra(src, dest):
+            dist=[float('inf')]*n 
+            minHeap = [(0, src)]
+            dist[src]=0
+            while minHeap:
+                d, u = heapq.heappop(minHeap)
+                if dist[u]<d: continue  # if distance is not latest , this avoids usage of visited array 
+                print(d, u, ' d, u ')
+                for (v, cost) in g[u]:
+                    if dist[v]>cost+dist[u]:
+                        dist[v]=cost+dist[u]
+                        heapq.heappush(minHeap, (dist[v], v))
+            return dist[dest]
+        print(g)
+        x = dijkstra(start, end)
+        return 1/(2**x)
