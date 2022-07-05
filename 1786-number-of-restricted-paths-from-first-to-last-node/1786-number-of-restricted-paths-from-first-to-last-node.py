74fb8b8230ea1c2da6a2486adf684457e23ec6f6
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-07-05 18:48:16 +0530

    Time: 3725 ms (27.51%), Space: 51.1 MB (87.77%) - LeetHub

--- 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.py
+++ 1786-number-of-restricted-paths-from-first-to-last-node/1786-number-of-restricted-paths-from-first-to-last-node.py
@@ -1,32 +1,42 @@
 class Solution:
     def countRestrictedPaths(self, n: int, edges: List[List[int]]) -> int:
-        if n == 1: return 0
-        graph = defaultdict(list)
-        for u, v, w in edges:
-            graph[u].append((w, v))
-            graph[v].append((w, u))
-
-        def dijkstra():  # Dijkstra to find shortest distance of paths from node `n` to any other nodes
-            minHeap = [(0, n)]  # dist, node
-            dist = [float('inf')] * (n + 1)
-            dist[n] = 0
-            while minHeap:
-                d, u = heappop(minHeap)
-                if d != dist[u]: continue
-                for w, v in graph[u]:
-                    if dist[v] > dist[u] + w:
-                        dist[v] = dist[u] + w
-                        heappush(minHeap, (dist[v], v))
-            return dist
-
-        @lru_cache(None)
-        def dfs(src):
-            if src == n: return 1  # Found a path to reach to destination
-            ans = 0
-            for _, nei in graph[src]:
-                if dist[src] > dist[nei]:
-                    ans = (ans + dfs(nei)) % 1000000007
-            return ans
+        # build graph
+        graph = defaultdict(dict)
+        for s, e, w in edges:
+            graph[s][e] = w
+            graph[e][s] = w
+            
+        # Distance to last node
+        dist_to_last = {node: float('inf') for node in range(1, n+1)}
+        dist_to_last[n] = 0
         
-        dist = dijkstra()
-        return dfs(1)
+        # number of restricted path to last node
+        count_path = {node: 0 for node in range(1, n+1)}
+        count_path[n] = 1
+        
+        # Dijkstra
+        ## calculated nodes
+        decided = set()
+        ## heap for candidate nodes
+        candidates = [(0,n)]
+        while candidates:
+            _, node = heapq.heappop(candidates)
+            if node in decided:
+                continue
+            decided.add(node)
+            for neigh in graph[node]:
+                if neigh in decided:
+                    continue
+                new_dist = graph[node][neigh] + dist_to_last[node]
+                
+                # update distance
+                if new_dist < dist_to_last[neigh]:
+                    dist_to_last[neigh] = new_dist
+                    heapq.heappush(candidates, (dist_to_last[neigh], neigh))
+                    
+                # update number of paths
+                if dist_to_last[neigh] > dist_to_last[node]:
+                    count_path[neigh] += count_path[node]
+
+
+        return count_path[1] % (10 ** 9 + 7)
