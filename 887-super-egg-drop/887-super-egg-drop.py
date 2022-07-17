c01a974ea266a4ee94734e74b29a4cf7e854c3ba
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-07-17 15:31:36 +0530

    Time: 140 ms (63.04%), Space: 22.8 MB (28.10%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.py
+++ 887-super-egg-drop/887-super-egg-drop.py
@@ -7,3 +7,21 @@ class Solution(object):
                 dp[floor][egg] = 1 + dp[floor - 1][egg - 1] + dp[floor - 1][egg]
                 if dp[floor][egg] >= n: return floor
         return -1
+#     Explanation
+
+# Drop eggs is a very classical problem.
+# Some people may come up with idea O(KN^2)
+# where dp[K][N] = 1 + max(dp[K - 1][i - 1], dp[K][N - i]) for i in 1...N.
+# However this idea is very brute force, for the reason that you check all possiblity.
+
+# So I consider this problem in a different way:
+# dp[M][K]means that, given K eggs and M moves,
+# what is the maximum number of floor that we can check.
+
+# The dp equation is:
+# dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1,
+# which means we take 1 move to a floor,
+# if egg breaks, then we can check dp[m - 1][k - 1] floors.
+# if egg doesn't breaks, then we can check dp[m - 1][k] floors.
+
+# dp[m][k] is the number of combinations and it increase exponentially to N
