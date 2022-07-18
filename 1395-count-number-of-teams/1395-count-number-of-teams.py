f37163e513f359635353b0b930c4d7ff6695c7a4
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-07-18 22:55:50 +0530

    Time: 1643 ms (37.11%), Space: 14.4 MB (13.45%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.py
+++ 1395-count-number-of-teams/1395-count-number-of-teams.py
@@ -1,12 +1,24 @@
 class Solution:
     def numTeams(self, rating: List[int]) -> int:
-        def count(arr):
-            dp = [0]*len(arr) #dp[i] records the number of element that lower than rating[i] on the left
-            ans = 0
-            for i in range(len(arr)):
-                for j in range(i):
-                    if arr[i] > arr[j]:
-                        dp[i]+=1
-                        ans+=dp[j]
-            return ans
-        return count(rating) + count(rating[::-1])
+        dp = [[1, 0, 0] for i in range(len(rating))]
+        
+        for i in range(1, len(rating)):
+            for j in range(i):
+                if rating[i] > rating[j]:
+                    dp[i][1] += dp[j][0]
+                    dp[i][2] += dp[j][1]
+        
+        a = sum(dp[i][2] for i in range(len(dp)))
+        #print(a)
+
+        dp = [[1, 0, 0] for i in range(len(rating))]
+        
+        for i in range(1, len(rating)):
+            for j in range(i):
+                if rating[i] < rating[j]:
+                    dp[i][1] += dp[j][0]
+                    dp[i][2] += dp[j][1]
+        
+        b = sum(dp[i][2] for i in range(len(dp)))
+        
+        return a + b
