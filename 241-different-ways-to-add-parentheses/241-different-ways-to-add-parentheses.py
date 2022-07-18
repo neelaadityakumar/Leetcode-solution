e6119946418cb4e7f6216ad32f319c492a83e7a6
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-07-18 19:35:25 +0530

    Time: 75 ms (21.59%), Space: 14.1 MB (52.87%) - LeetHub

---
+++ 241-different-ways-to-add-parentheses/241-different-ways-to-add-parentheses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def solve(self, s):
+        if s.isdigit():
+            return [int(s)]
+        if s in self.d:
+            return self.d[s]
+        ans = []
+        for i in range(len(s)):
+            if s[i] in ["+", "-", "*"]:
+                left = self.solve(s[:i])
+                right = self.solve(s[i+1:])
+                for l in left:
+                    for r in right:
+                        if s[i] == "-": ans.append(l-r)
+                        if s[i] == "+": ans.append(l+r)
+                        if s[i] == "*": ans.append(l*r)
+        self.d[s] = ans
+        return ans
+    
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        self.d = {}
+        return self.solve(expression)
+        
