0838173695df57fb17ffc3a6616459a39005bbe8
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-07-20 18:46:33 +0530

    Time: 70 ms (19.34%), Space: 14 MB (10.28%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.py
@@ -0,0 +1,16 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        max_val = max(heights)
+        
+        # Create frequency table
+        freq = [0] * (max_val + 1)
+        for num in heights: freq[num] += 1
+        for num in range(1, len(freq)): freq[num] += freq[num-1]
+
+        # Create places table
+        places = [0] * len(heights)
+        for num in heights:
+            places[freq[num]-1] = num
+            freq[num] -= 1
+
+        return sum([a!=b for a, b in zip(heights, places)])
