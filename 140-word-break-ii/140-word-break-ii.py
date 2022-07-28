5ea0c5a0a893f9751289df11ed791b19d4cd1906
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-07-28 15:19:29 +0530

    Time: 58 ms (19.60%), Space: 13.9 MB (81.48%) - LeetHub

---
+++ 140-word-break-ii/140-word-break-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        return self.dfs(s, set(wordDict), {})
+    
+    def dfs(self, s, d, m):
+        if s in m: # memorize
+            return m[s]
+        if not s:
+            return [""]
+        res = []
+        for i in range(1, len(s)+1):
+            if s[:i] in d:
+                for word in self.dfs(s[i:], d, m):
+                    res.append(s[:i] + (" " if word else "") + word)
+        m[s] = res
+        return res
