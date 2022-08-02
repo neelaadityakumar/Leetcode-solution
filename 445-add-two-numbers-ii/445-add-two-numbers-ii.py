7fba4eec149cf211f72d43f187da0316a166a519
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-08-02 21:41:55 +0530

    Time: 136 ms (21.50%), Space: 14 MB (47.35%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.py
@@ -12,10 +12,11 @@ class Solution:
         while l2:
             stack2.append(l2.val)
             l2 = l2.next
-        carry, head = 0, None
+        carry, curr,next = 0, None,None
         while stack1 or stack2 or carry:
             d1 = stack1.pop() if stack1 else 0
             d2 = stack2.pop() if stack2 else 0
             carry, val = divmod(d1+d2+carry, 10)
-            head = ListNode(val, head)
-        return head
+            curr = ListNode(val, next)
+            next=curr
+        return curr
