9fd52e0107b81a7bc233e0e02517b574b7c4a386
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-08-03 18:09:32 +0530

    Time: 52 ms (50.64%), Space: 13.9 MB (16.04%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        result = 0
+        symbol=1
+        if x < 0:
+            symbol = -1
+            x = -x
+        while x:
+            result = result * 10 + x % 10
+            x //= 10
+
+        return 0 if result > pow(2, 31) else result * symbol
