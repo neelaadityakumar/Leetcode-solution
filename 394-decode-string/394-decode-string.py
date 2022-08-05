971ffde57cda0716d885de47c5a6cb86f9e865bf
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-08-05 23:21:11 +0530

    Time: 60 ms (14.83%), Space: 13.9 MB (71.86%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,20 +1,29 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-        res,num = "",0
-        st = []
-        for c in s:
-            if c.isdigit():
-                num = num*10+int(c)    
-            elif c=="[":
-                st.append(res)
-                st.append(num)
-                res=""
-                num=0
-            elif c=="]":
-                pnum = st.pop()
-                pstr = st.pop()
-                res = pstr + pnum*res
-            else:
-                res+=c
+        """
+    When we hit an open bracket, we know we have parsed k for the contents of the bracket, so 
+    push (current_string, k) to the stack, so we can pop them on closing bracket to duplicate
+    the enclosed string k times.
+    """
+        stack = []
+        current_string = ""
+        k = 0
 
-        return res
+        for char in s:
+            if char == "[":
+                # Just finished parsing this k, save current string and k for when we pop
+                stack.append((current_string, k))
+                # Reset current_string and k for this new frame
+                current_string = ""
+                k = 0
+            elif char == "]":
+                # We have completed this frame, get the last current_string and k from when the frame 
+                # opened, which is the k we need to duplicate the current current_string by
+                last_string, last_k = stack.pop(-1)
+                current_string = last_string + last_k * current_string
+            elif char.isdigit():
+                k = k * 10 + int(char)
+            else:
+                current_string += char
+
+        return current_string
