3dcc0b9cb8b0e1af923f4faf8a3c4eabcf73f186
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-08-07 13:22:24 +0530

    Time: 1127 ms (50.00%), Space: 28.6 MB (100.00%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.py
@@ -4,13 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        tot = len(nums) * (len(nums) - 1) // 2
-        good = 0
-        dic = {}
+        n = len(nums)
+        dic={}
+        for i in range(n):
+            if nums[i] - i not in dic:
+                dic[nums[i] - i]=0
+            dic[nums[i] - i]+=1
+
+        ans = n * (n - 1) // 2
+        for x in dic:
+            if dic[x] > 1:
+                ans -= dic[x] * (dic[x] - 1) // 2
         
-        for i,num in enumerate(nums):
-            v = i - num
-            good += dic.get(v, 0)
-            dic[v] = dic.get(v, 0) + 1
-        
-        return tot - good
+        return ans
