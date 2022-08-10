392f309db35bca9745624cfc5939e4c286ca764d
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-08-11 00:50:43 +0530

    Time: 63 ms (59.45%), Space: 14.8 MB (78.63%) - LeetHub

---
+++ 260-single-number-iii/260-single-number-iii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        xor = 0
+        a = 0
+        b = 0
+        for num in nums:
+            xor ^= num
+        mask = 1
+        while(xor&mask == 0):
+            mask = mask << 1
+        for num in nums:
+            if num&mask:
+                a ^= num
+            else:
+                b ^= num
+        return [a, b]
+    # You first xor all numbers and then find a bit that is different in those two distinct numbers. This bit is used to divide all numbers into two group. Finally xor them individually, the two numbers left are the two distinct numbers.
