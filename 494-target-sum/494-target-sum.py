8b0bdae9b16d7e43d3142eaa8e8d72b4178a5070
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-09-14 23:31:19 +0530

    Time: 396 ms (69.38%), Space: 18.1 MB (55.00%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -1,18 +1,47 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        dp = {}
-        def dfs(i,target):
-            if (i,target) in dp:
-                return dp[(i,target)]
-            if i >= n:
-                if target == 0:
-                    return 1
-                else:
-                    return 0
-            # sum of 2 ways 
-            # 1st way => consider i th index as + nums[i] => ask dfs() to move to next index and reduce the target
-            # 2nd way => consider i th index as - nums[i] => ask dfs() to move to next index and increase the target
-            dp[(i,target)] = dfs(i+1,target - nums[i]) + dfs(i+1,target + nums[i]) 
-            return dp[(i,target)]
-        return dfs(0,target)
+        # n = len(nums)
+        # dp = {}
+        # def dfs(i,target):
+        #     if (i,target) in dp:
+        #         return dp[(i,target)]
+        #     if i >= n:
+        #         if target == 0:
+        #             return 1
+        #         else:
+        #             return 0
+        #     # sum of 2 ways 
+        #     # 1st way => consider i th index as + nums[i] => ask dfs() to move to next index and reduce the target
+        #     # 2nd way => consider i th index as - nums[i] => ask dfs() to move to next index and increase the target
+        #     dp[(i,target)] = dfs(i+1,target - nums[i]) + dfs(i+1,target + nums[i]) 
+        #     return dp[(i,target)]
+        # return dfs(0,target)
+        # helper
+        def f(i, trgtSum):
+
+            if i == len(nums) and trgtSum == 0: # --- NOTE [1]
+                return 1
+
+            # if trgtSum == 0: #  does not work when a 0 is trailing +ve integers
+            #     return 1
+
+            if i == len(nums):
+                return 0
+
+            if trgtSum < 0:
+                return 0
+
+            if (i, trgtSum) in memo:
+                return memo[(i, trgtSum)]
+
+            result = f(i+1, trgtSum-nums[i]) + f(i+1, trgtSum)
+            memo[(i,trgtSum)] = result
+            return result
+
+        # main
+        nums.sort()
+        memo = {}
+        if (sum(nums) + target) % 2 != 0:
+            return 0
+        trgtSum = (sum(nums) + target) // 2
+        return f(0, trgtSum)
