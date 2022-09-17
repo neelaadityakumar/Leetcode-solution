907c3c9180edcbfda8ee68a0c40ba318ec94f949
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-09-17 14:34:32 +0530

    Time: 344 ms (97.41%), Space: 15.6 MB (13.70%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def shipWithinDays(self, weights, D):
+        """
+        :type weights: List[int]
+        :type days: int
+        :rtype: int
+        """
+        def feasible(capacity):
+            days = 1
+            total = 0
+            for weight in weights:
+                total += weight
+                if total > capacity:  # too heavy, wait for the next day
+                    total = weight
+                    days += 1
+                    if days > D:  # cannot ship within D days
+                        return False
+            return True
+
+        left, right = max(weights), sum(weights)
+        while left < right:
+            mid = left + (right - left) // 2
+            if feasible(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
