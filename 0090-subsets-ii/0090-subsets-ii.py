dbfaae712ff8d7b821a729fe9838131a8dbf6465
neelaadityakumar <51313557+neelaadityakumar@users.noreply.github.com>
2022-11-26 13:45:14 +0530

    Time: 79 ms (34.83%), Space: 14.1 MB (49.81%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        self.dfs(sorted(nums), 0, [], res)
+        return res
+
+    def dfs(self, nums, index, path, res):
+        res.append(path)
+        for i in range(index, len(nums)):
+            if i > index and nums[i] == nums[i - 1]: 
+                continue  # Skip duplicates
+            self.dfs(nums, i+1, path+[nums[i]], res)
